eb50e6f3ce9b3f49928cf548488f1ea9aa94ec05
Izhar-Ul-Haq <iizharulhaq9@gmail.com>
2023-05-08 16:18:58 +0500

    Attributes Scraping in Python

---
+++ attributesScraping.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://webscraper.io/test-sites/e-commerce/allinone"
+r = requests.get(url)
+print(r)
+
+soup = BeautifulSoup(r.text, "lxml")
+print(soup)
+
+head = soup.header
+print(head)
+headAttributes = head.attrs
+print(head.attrs)
+print(headAttributes['class'])
+print(headAttributes['role'])
+
+
+print(soup.div.attrs)
